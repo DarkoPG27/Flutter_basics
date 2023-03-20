b0aaaaff6c13bf2b4ba8524abd07e50a06a77801
DarkoPG27 <darkobrnovic27@gmail.com>
2023-03-20 12:24:22 +0100

    Splitting app into three widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,19 @@
 import 'package:flutter/material.dart';
+import './text_contorl.dart';
 
 main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Assignment 1'),
-        ),
-        body: Column(
-          children: [
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Change text'),
-            ),
-            Text('This is the first assignment!'),
-          ],
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Assignment 1'),
       ),
-    );
+      body: TextControl(),
+    ));
   }
 }
